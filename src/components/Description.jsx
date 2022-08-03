d6a1ce9518f7bc5bc0b547e8b146df49911e96e8
nagarajtolearn <nagarajtolearn@gmail.com>
2022-08-03 17:47:02 +0530

    bug fix

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -2,9 +2,8 @@ import React from "react";
 import { FaArrowDown, FaArrowUp, FaWind } from "react-icons/fa";
 import { BiHappy } from "react-icons/bi";
 import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
-
 import "./description.css";
-import { data } from "autoprefixer";
+
 const Description = ({ weather, units }) => {
   const tempUnit = units === "metric" ? "°C" : "°F";
   const windUnit = units === "metric" ? "m/s" : "m/h";
